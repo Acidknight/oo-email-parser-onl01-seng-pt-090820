6d094f30943533e01814fd8d30a3f03713e3706c
Anthony Leon <acidknights89@gmail.com>
2020-09-25 00:59:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,6 +3,10 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :name, 
+  attr_accessor :name, :csv_emails
+  
+  def initialize
+    
+  end
   
 end
